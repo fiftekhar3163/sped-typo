b20a61d8304fab3362cb792bffc4ae7749573e85
Mdfahimiftekhar <iftekharfahim3163@gmail.com>
2024-04-05 22:16:59 -0500

    final update

--- assets/script/script.js
+++ assets/script/script.js
@@ -119,6 +119,7 @@ let currentWordIndex = 0;
 let hits = 0;
 let timer;
 let isPlaying = false;
+let gameScores = [];
 
 const wordDisplay = document.getElementById("word");
 const userInput = document.getElementById("user-input");
@@ -136,26 +137,53 @@ const buttonSound = new Audio("./assets/audio/button-click.wav");
 const pingSound = new Audio("./assets/audio/ping-sound.mp3");
 const backgroundSound = new Audio("./assets/audio/bg.wav");
 
+const highScoresList = document.getElementById("highScoresList");
+
+function updateHighScore() {
+  const previousScores = localStorage.getItem("highScores");
+  if (!previousScores) {
+    const noGameBefore = `No games have been played`;
+    highScoresList.innerHTML = `<li>${noGameBefore}</li>`;
+  } else {
+    const scoreArr = JSON.parse(previousScores);
+    highScoresList.innerHTML = "";
+    scoreArr.map((p, ind) => {
+      const gameId = ind + 1;
+      const gameHits = p.hits;
+      const gamePercentage = p.percentage;
+      const li = document.createElement("li");
+      li.className = "highScoresItem";
+      li.innerHTML = `<span>#${gameId}</span> <span>${gameHits} words</span><span> ${gamePercentage}</span>`;
+      //append to highScoresList
+      highScoresList.appendChild(li);
+    });
+  }
+}
+
+//disable the input
+userInput.disabled = true;
+
+updateHighScore();
+
 // Function to start the game
 function startGame() {
-
   backgroundSound.loop = true;
   backgroundSound.volume = 0.3;
-  setTimeout(function(){
+  setTimeout(function () {
     backgroundSound.play();
-  },1000)
+  }, 1000);
   buttonSound.play();
   const max = 90;
   const min = 0;
   currentWordIndex = Math.floor(Math.random() * (max - min + 1)) + min;
 
   //focus to input;
+  userInput.disabled = false;
   userInput.focus();
+
   if (!isPlaying) {
     isPlaying = true;
-    startBtn.disabled = true;
-    startBtn.textContent = "Playing...";
-
+    startBtn.textContent = "Restart";
     // Start the timer
     let seconds = 20;
     timer = setInterval(() => {
@@ -171,6 +199,15 @@ function startGame() {
 
     // Check input
     userInput.addEventListener("input", checkInput);
+  } else {
+    //restart the game
+    buttonSound.play();
+    clearInterval(timer);
+    isPlaying = false;
+    startBtn.disabled = false;
+    startBtn.textContent = "Start";
+    userInput.removeEventListener("input", checkInput);
+    endGame();
   }
 }
 
@@ -209,31 +246,62 @@ function endGame() {
   userInput.removeEventListener("input", checkInput);
 
   // Calculate percentage
-  const percentage = Math.floor((hits / words.length) * 100);
+  const percentage = ((hits / words.length) * 100).toFixed(1) + "%";
 
   // Create Score object
   const date = new Date().toLocaleDateString();
-  const score = new Score(date, hits, percentage);
+  const score = {
+    hits,
+    date,
+    percentage,
+  };
 
-  popupDate.innerText = `Date: ${score.date}`;
-  popupHits.innerText = `Hits: ${score.hits}`;
-  popupPercentage.innerText = `Percentage: ${score.percentage}%`;
+  popupDate.innerText = `Date: ${date}`;
+  popupHits.innerText = `Hits: ${hits}`;
+  popupPercentage.innerText = `Percentage: ${percentage}`;
+
+  //pushing score data to array;
+  gameScores.push(score);
+
+  //setting scores into localstorage
+
+  const previousScores = localStorage.getItem("highScores");
+
+  if (previousScores) {
+    const preScoreArr = JSON.parse(previousScores);
+    console.log(preScoreArr, gameScores);
+    //assigning to gameScore array
+    gameScores = [...gameScores, ...preScoreArr]
+      .sort((a, b) => b.hits - a.hits)
+      .splice(0, 9);
+  }
+
+  localStorage.setItem("highScores", JSON.stringify(gameScores));
+
+  //empty the gameScores array
+  gameScores = [];
 
   openPopup();
+  updateHighScore();
+
   // Reset game variables
   currentWordIndex = 0;
   hits = 0;
-  scoreDisplay.textContent = "";
 }
 
 function openPopup() {
   popup.style.display = "block"; // Display the popup
+  userInput.disabled = true;
+  userInput.value = "";
+  timerDisplay.textContent = "20";
 }
 
 // Function to close the popup
 function closePopup() {
-  window.location.reload();
   popup.style.display = "none"; // Hide the popup
+  scoreDisplay.innerHTML = "Hits:0";
+  wordDisplay.innerText = "";
+  backgroundSound.pause();
 }
 
 // Event listener for start button
